c10bae7d20ec1bd0c424d3bc26a56fef5a8764ab
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-10-04 14:15:08 +0600

    async thunk created

---
+++ src/features/transaction/transactionSlice.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import {
+  addTransaction,
+  deleteTransaction,
+  editTransaction,
+  getTransactions,
+} from "./transactionAPI";
+
+const initialState = {
+  transactions: [],
+  isLoading: false,
+  isError: false,
+  error: "",
+};
+
+export const fetchTransactions = createAsyncThunk(
+  "transaction/fetchTransactions",
+  async () => {
+    const transactions = await getTransactions();
+    return transactions;
+  }
+);
+
+export const createTransaction = createAsyncThunk(
+  "transaction/createTransaction",
+  async (data) => {
+    const transaction = await addTransaction(data);
+    return transaction;
+  }
+);
+
+export const changeTransaction = createAsyncThunk(
+  "transaction/changeTransaction",
+  async ({ id, data }) => {
+    const transaction = await editTransaction(id, data);
+    return transaction;
+  }
+);
+
+export const removeTransaction = createAsyncThunk(
+  "transaction/removeTransaction",
+  async (id) => {
+    const transaction = await deleteTransaction(id);
+    return transaction;
+  }
+);
