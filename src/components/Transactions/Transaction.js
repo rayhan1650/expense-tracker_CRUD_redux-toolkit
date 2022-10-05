572e09778601a8d8c243f4ea4be52d829d4e8426
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-10-05 17:26:25 +0600

    fetch transaction completed

--- src/components/Transactions/Transaction.js
+++ src/components/Transactions/Transaction.js
@@ -2,12 +2,13 @@ import React from "react";
 import editImage from "../../assets/images/edit.svg";
 import deleteImage from "../../assets/images/delete.svg";
 
-const Transaction = () => {
+const Transaction = ({ transaction }) => {
+  const { name, amount, type } = transaction || {};
   return (
-    <li className="transaction income">
-      <p>Earned this month</p>
+    <li className={`transaction ${type}`}>
+      <p>{name}</p>
       <div className="right">
-        <p>৳ 100</p>
+        <p>৳ {amount}</p>
         <button className="link">
           <img className="icon" src={editImage} alt="edit" />
         </button>
